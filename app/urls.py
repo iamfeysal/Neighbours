36c0caecfdf0a368fff30b00d70e1aa4c9b2f990
feysal-Ibrahim <addictivefazman@gmail.com>
2018-10-20 15:22:13 +0300

    create models

--- app/urls.py
+++ app/urls.py
@@ -2,3 +2,8 @@ from django.conf.urls import url
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+
+urlpatterns=[
+    # /app/
+    url( r'^$' , views.IndexView.as_view( ) ,name='index'),
+    ]
