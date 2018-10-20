97a21a7976683198ddee71becfcb67884bf008cc
feysal-Ibrahim <addictivefazman@gmail.com>
2018-10-20 16:06:34 +0300

    create first template

--- app/views.py
+++ app/views.py
@@ -5,9 +5,15 @@ from django.core.urlresolvers import reverse_lazy
 from django.shortcuts import render,redirect
 from django.contrib.auth import  authenticate,login
 from django.views.generic import View
+from .models import *
 
 # Create your views here.
 class IndexView(generic.ListView):
-    template_name = 'music/index.html'
-    context_object_name = 'all_albums'
+    template_name = 'index.html'
+    context_object_name = 'Neighbourhood'
+
+    def get_queryset(self):
+        return Neighbourhood.objects.all( )
+
+
 
