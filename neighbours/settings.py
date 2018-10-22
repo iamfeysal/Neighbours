b3309d6d1432d1e089cf11752f5ce5cdafa7d144
feysal-Ibrahim <addictivefazman@gmail.com>
2018-10-22 09:57:41 +0300

    removed tinymice

--- neighbours/settings.py
+++ neighbours/settings.py
@@ -31,8 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'app.apps.AppConfig' ,
-    'bootstrap4',
+    'app.apps.AppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
