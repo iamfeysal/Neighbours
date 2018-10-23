3442afc41a7c6a65bc1e5625ff0ff8f266fd3410
feysal-Ibrahim <addictivefazman@gmail.com>
2018-10-23 09:41:31 +0300

    lastly

--- neighbours/settings.py
+++ neighbours/settings.py
@@ -9,16 +9,53 @@ https://docs.djangoproject.com/en/1.11/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
-
 import os
+import django_heroku
+import dj_database_url
+from decouple import config , Csv
 
+MODE=config( "MODE" , default="dev" )
+SECRET_KEY=config( 'SECRET_KEY' )
+DEBUG=config( 'DEBUG' , default=False , cast=bool )
+# development
+if config( 'MODE' ) == "dev":
+    DATABASES={
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2' ,
+            'NAME': config( 'DB_NAME' ) ,
+            'USER': config( 'DB_USER' ) ,
+            'PASSWORD': config( 'DB_PASSWORD'  ) ,
+            'HOST': config( 'DB_HOST' ) ,
+            'PORT': '' ,
+        }
+
+    }
+# production
+else:
+    DATABASES={
+        'default': dj_database_url.config(
+            default=config( 'DATABASE_URL' )
+        )
+    }
+
+db_from_env=dj_database_url.config( conn_max_age=500 )
+DATABASES['default'].update( db_from_env )
+
+ALLOWED_HOSTS=config( 'ALLOWED_HOSTS' , cast=Csv( ) )
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR=os.path.dirname( os.path.dirname( os.path.abspath( __file__ ) ) )
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT=os.path.join( BASE_DIR , 'staticfiles' )
+STATIC_URL='/static/'
 
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS=(
+    os.path.join( BASE_DIR , 'static' ) ,
+)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'i=i4k6apujcq9ugqla9rkm61-rwnkvbrk2f34u1b*p*=4r&@ln'
 
@@ -50,6 +87,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware' ,
 ]
 
 ROOT_URLCONF = 'neighbours.urls'
@@ -77,10 +115,12 @@ WSGI_APPLICATION = 'neighbours.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
+DATABASES={
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql' ,
+        'NAME': 'neighbours' ,
+        'USER': 'feisal' ,
+        'PASSWORD': '1234' ,
     }
 }
 
@@ -117,14 +157,16 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+# https://docs.djangoproject.com/en/1.11/howto/static-files/
+
+STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # https://docs.djangoproject.com/en/1.11/howto/static-files/https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 # configuring the location for media
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL='/media/'
+MEDIA_ROOT=os.path.join( BASE_DIR , 'media' )
+
+# Configure Django App for Heroku.
+django_heroku.settings( locals( ) )
